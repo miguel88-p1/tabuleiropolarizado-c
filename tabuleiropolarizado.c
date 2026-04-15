67d1ef570d652d598ebcf2c40f3eb2b6d0aa7a19
miguel88-p1 <mellogomes.miguel88@gmail.com>
2026-04-15 20:16:49 -0300

    Add files via upload

---
+++ tabuleiropolarizado.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void tabuleiropolarizado() {
+    printf("O O O\n");
+    printf("O X X\n");
+    printf("X X X\n");
+}
+
+int main() {
+    tabuleiropolarizado();
+    return 0;
+}
